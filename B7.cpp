53cd8b688ec3291d30238044e613c9056c6ae304
Dinhlu24 <dinhlunhut@gmail.com>
2024-12-30 08:26:52 +0700

    Update B7.cpp

--- B7.cpp
+++ B7.cpp
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #define maxSize 100
 
-int n,m;
 int matrix[maxSize][maxSize]={0};
 
-void matrixInput();
-void matrixOutput();
+void matrixInput(int n,int m);
+void matrixOutput(int n,int m);
 
 int main(){
-	matrixInput();
-	matrixOutput();
-}
-
-void matrixInput(){
+	int n,m;
 	printf("Moi ban nhap vao so hang: ");	scanf("%d",&n);
 	printf("Moi ban nhap vao so cot: ");	scanf("%d",&m);
 	
+	matrixInput(n,m);
+	matrixOutput(n,m);
+}
+
+void matrixInput(int n,int m){
 	for(int i=0;i<n;i++){
 		for(int j=0;j<m;j++){
 			printf("Matrix[%d]: ",i);
@@ -24,7 +24,7 @@ void matrixInput(){
 	}
 }
 
-void matrixOutput(){
+void matrixOutput(int n,int m){
 	printf("Day la ma tran cua ban:\n");
 	for(int i=0;i<n;i++){
 		for(int j=0;j<m;j++){
